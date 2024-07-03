4845050ad6d885703d75a786849efc0c36ccf827
Vinhle1710 <117881066+Vinhle1710@users.noreply.github.com>
2024-07-03 09:36:52 +0700

    hj

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    int num;
+    cout << "Enter an interger: ";
+    std::cin >> num;
+    cout << "got number = " << num << "\n";
+    return 0;
+}
